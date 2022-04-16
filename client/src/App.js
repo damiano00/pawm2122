fba4d4db8f035ce7d1335f7bec9a612da05e8cd0
marcoquadrini <marco.quadrini@studenti.unicam.it>
2022-04-16 17:01:06 +0200

    Interazione con firebase realtime db aggiunta

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data , setData] = React.useState(null);
+  const [name , setName] = React.useState(null);
+  const [surname , setSurname] = React.useState(null);
 
   React.useEffect(()=>{
     fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
+    .then(response => response.json())
+    .then((data) => {setName(JSON.stringify(data.nome)); setSurname(JSON.stringify(data.cognome));  } );
   }, []);
-
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{name}</p>
+        <p>{surname}</p>
       </header>
     </div>
   );
+
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,71 @@
-const express = require("express");
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+import { ref, set ,onValue } from "firebase/database";
+import express from 'express';
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+
+// TODO: Replace with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCuAVHkBP56H1nGggt1-DjXlALu4M_qPiY",
+    authDomain: "todolistapppawm.firebaseapp.com",
+    databaseURL: "https://todolistapppawm-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todolistapppawm",
+    storageBucket: "todolistapppawm.appspot.com",
+    messagingSenderId: "849297804624",
+    appId: "1:849297804624:web:e022b5481fb776579bf055",
+    measurementId: "G-K47X0QTK5X"
+  };
+
+const fb = initializeApp(firebaseConfig);
+
+
+
+
+function writeUserData(userId, name, email, imageUrl) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
+writeUserData("1","Marco","marcoquadrinimsp@gmail.com","www.google.com")
+
+
+
+  // Get a reference to the database service
+const database = ref( getDatabase() );
+var  data;
+onValue(database,(snapshot) => {
+    data = snapshot.child("users/1/username").val();
+    console.log(data)
+  });
   
+
+
+
+
+
+
+
+
+
+
+
+app.get("/api", (req, res) => {
+    res.json({ 
+        nome: data,
+        email : "quadrini"
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
